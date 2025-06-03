17b3764420a4ce9c84d4584ab35ad922be553ba3
Fred Condo <fcondo@quinn.com>
2025-06-03 10:05:23 -0700

    Add standard custom aliases to chezmoi
    Use local.zsh for truly local stuff

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,4 @@
+alias  _='doas'
+alias zl='zfs list -o name,creation,used,available,reservation,canmount,mounted,mountpoint,compressratio,compression'
+alias zla='zfs list -o name,creation,used,available,reservation,canmount,mounted,mountpoint,compressratio,compression -t all'
+alias zls='zfs list -o name,creation,used,available,reservation,canmount,mounted,mountpoint,compressratio,compression -t snapshot'
